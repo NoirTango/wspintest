76868af7ce08d5a657dc7d0ee780221a09832d01
jacek <j.c.wojdel@gmail.com>
2016-10-22 01:18:25 +0200

    Initial models are in

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Crag(models.Model):
+    name = models.TextField()
+    country = models.TextField()
+
+class Sector(models.Model):
+    name = models.TextField()
+    crag = models.ForeignKey(Crag)
+
+class Route(models.Model):
+    name = models.TextField()
+    sector = models.ForeignKey(Sector)

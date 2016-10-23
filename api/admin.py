ea7c55cf48b363277e48095691ac57332e92518a
jacek <j.c.wojdel@gmail.com>
2016-10-23 19:41:09 +0200

    Added per-user climbing record

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Crag, Sector, Route
+from .models import Crag, Sector, Route, ClimbRecord
 
 # Register your models here.
 
 admin.site.register(Crag)
 admin.site.register(Sector)
 admin.site.register(Route)
+admin.site.register(ClimbRecord)
--- api/models.py
+++ api/models.py
@@ -1,14 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+
 class Crag(models.Model):
     name = models.TextField()
     country = models.TextField()
+
+    def __str__(self):
+        return '{} - {}'.format(self.name, self.country)
+
 
 class Sector(models.Model):
     name = models.TextField()
     crag = models.ForeignKey(Crag)
 
+    def __str__(self):
+        return '{} - {}'.format(self.name, self.crag.name)
+
+
 class Route(models.Model):
     name = models.TextField()
     sector = models.ForeignKey(Sector)
+    grade = models.CharField(max_length=20)
+
+    def __str__(self):
+        return '{} - {} - {}'.format(self.name, self.grade, self.sector)
+
+
+class ClimbRecord(models.Model):
+    route = models.ForeignKey(Route)
+    date = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=CASCADE)
+
+    def __str__(self):
+        return '{} - {} / {}'.format(self.route.name, self.route.grade, self.date)
